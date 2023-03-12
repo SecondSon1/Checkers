89188bf8f8f5314fa82c10fe3bbcac9d381422c1
SecondSon1 <bulatunusov100@gmail.com>
2023-03-12 22:18:52 +0400

    changed something

--- src/gui/scenes/game.hpp
+++ src/gui/scenes/game.hpp
@@ -30,6 +30,21 @@ class GameScene : public Scene {
               current_square_color_(247, 82, 82, 100),
               possible_move_square_color_(201, 22, 22, 100) {}
 
+  GameScene(std::shared_ptr<Player> white_player, std::shared_ptr<Player> black_player,
+            const sf::Font & font, std::string config)
+      : white_human_player_(std::dynamic_pointer_cast<Human>(white_player)),
+        black_human_player_(std::dynamic_pointer_cast<Human>(black_player)), move_entered_(false),
+        sequential_move_in_progress_(false), animation_state_(AnimationState::kIdle),
+        animated_move_(Position(0), Position(1)),
+        animated_currently_moving_(animated_move_), animated_move_speed_(0.01f),
+        game_(std::move(white_player), std::move(black_player), std::move(config)),
+        font_(font), white_square_color_(238, 238, 215, 255),
+        black_square_color_(129, 147, 99, 255),
+        white_piece_color_(248, 248, 248, 255),
+        black_piece_color_(85, 83, 82, 255),
+        current_square_color_(247, 82, 82, 100),
+        possible_move_square_color_(201, 22, 22, 100) {}
+
  private:
   void Init() override;
   void Draw(const sf::RenderWindow & window, sf::RenderTexture & texture) override;
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "gui/scenes/config.hpp"
 #include "gui/window.hpp"
+#include "gui/scenes/game.hpp"
+#include "player/bot/calculating_bot/calculating_bot.hpp"
 
 int main() {
 
@@ -7,6 +9,10 @@ int main() {
   assert(font.loadFromFile("../BPmono.ttf"));
 
   std::unique_ptr<Scene> game_scene_ptr = std::make_unique<ConfigScene>(font);
+//  std::unique_ptr<Scene> game_scene_ptr = std::make_unique<GameScene>(
+//      std::make_unique<Human>(PieceColor::kWhite),
+//      std::make_unique<CalculatingBot>(PieceColor::kBlack),
+//      font, ".....B.W.......w................w");
 
   sf::Color bg_color(0xE8, 0xDE, 0xD1, 0xFF);
   std::shared_ptr<Window> game = std::make_shared<Window>(1000, 1200, "Checkers",
